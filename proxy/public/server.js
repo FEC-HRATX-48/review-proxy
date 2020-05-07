fbefffebaff1e8fdc029d8c0fd38b4bfbac52f3f
Kody Maus <auto.mausx@gmail.com>
2020-05-07 11:15:27 -0600

    got proxy server running, configured webpack for proxy

---
+++ proxy/public/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const PORT = 9000;
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use (bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../dist')))
+
+
+app.listen(PORT, () => {
+  console.log(`WE LIVE BABY, ON PORT: ${PORT}`)
+})
